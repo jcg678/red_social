a6a48b60c704a7f6660d6b182b7969d2a20512bf
jcg678 <jcg678@hotmail.es>
2017-02-07 15:46:00 +0100

    rutas de cada controlador por separado

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller{
+    public function loginAction(Request $request){
+        echo "Preuba";
+        die();
+    }
+}
