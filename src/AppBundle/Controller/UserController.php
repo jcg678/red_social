7d58d233a829a23115fe25073fd22eb8ad6136b0
jcg678 <jcg678@hotmail.es>
2017-02-10 23:35:38 +0100

    formulario type y vista

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -5,11 +5,22 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use BackendBundle\Entity\User;
+use AppBundle\Form\RegisterType;
 
 class UserController extends Controller{
     public function loginAction(Request $request){
         return $this->render('AppBundle:User:login.html.twig',[
             "x"=> "hola"
+        ]);
+    }
+
+    public function registerAction(Request $request){
+        $user = new User();
+        $form = $this->createForm(RegisterType::class,$user);
+
+        return $this->render('AppBundle:User:register.html.twig',[
+            "form"=>$form->createView()
         ]);
     }
 }
