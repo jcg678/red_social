cad1e7bd67a9193634acb4208cf208b92149719a
jcg678 <jcg678@hotmail.es>
2017-02-13 15:53:26 +0100

    mensajes flash

--- src/BackendBundle/Entity/User.php
+++ src/BackendBundle/Entity/User.php
@@ -57,6 +57,23 @@ class User
      */
     private $image;
 
+    public function  getUsername()
+    {
+        return $this->email;
+    }
+    public function getSalt(){
+        return null;
+    }
+
+    public function getRoles(){
+        return $this->getRole();
+    }
+    public function eraseCredentials(){
+
+    }
+    public function __toString(){
+        return $this->name;
+    }
 
     /**
      * Get id
