3e427729fcc47c561bbf2b3f7a4c602d3b4eee22
jcg678 <jcg678@hotmail.es>
2017-03-05 13:31:14 +0100

    publicacion formulario

--- src/AppBundle/Controller/PublicationController.php
+++ src/AppBundle/Controller/PublicationController.php
@@ -5,10 +5,16 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
+use AppBundle\Form\PublicationType;
+use BackendBundle\Entity\Publication;
 class PublicationController extends Controller{
     public function indexAction(Request $request){
+        $em= $this->getDoctrine()->getManager();
+        $publication = new Publication();
+        $form = $this->createForm(PublicationType::class,$publication);
 
-        return $this->render('AppBundle:Publication:home.html.twig');
+        return $this->render('AppBundle:Publication:home.html.twig',[
+            'form'=>$form->createView()
+        ]);
     }
 }
