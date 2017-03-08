8972aa4ee8cce5f0fe5cd7e615ac694cca7b1489
jcg678 <jcg678@hotmail.es>
2017-03-08 16:49:32 +0100

    listadas las publicaciones

--- src/AppBundle/Controller/PublicationController.php
+++ src/AppBundle/Controller/PublicationController.php
@@ -66,9 +66,36 @@ class PublicationController extends Controller{
             $this->session->getFlashBag()->add("status",$status);
             return $this->redirectToRoute('home_publications');
         }
-
+       $publications = $this->getPublications($request);
         return $this->render('AppBundle:Publication:home.html.twig',[
-            'form'=>$form->createView()
+            'form'=>$form->createView(),
+           'pagination' => $publications
         ]);
+    }
+
+    public function getPublications($request){
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+        $publicatios_repo = $em->getRepository('BackendBundle:Publication');
+        $following_repo = $em->getRepository('BackendBundle:Following');
+
+        $following = $following_repo->findBy(array('user'=>$user));
+        $following_array= array();
+        foreach($following as $follow){
+            $following_array[]= $follow->getFollowed();
+        }
+        $query = $publicatios_repo->createQueryBuilder('p')
+            ->where('p.user = (:user_id) OR p.user IN (:following)')
+            ->setParameter('user_id',$user->getId())
+            ->setParameter('following', $following_array)
+            ->orderBy('p.id',"DESC")
+            ->getQuery();
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page',1),
+            5
+        );
+        return $pagination;
     }
 }
