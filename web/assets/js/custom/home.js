423fcd9c8e10204d97899319b352c4e116b89991
jcg678 <jcg678@hotmail.es>
2017-03-12 16:06:39 +0100

    scroll infinito en la publicacioes

---
+++ web/assets/js/custom/home.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+   var ias = jQuery.ias({
+      container: '#timeline .box-content',
+      item: '.publication-item',
+      pagination: '#timeline .pagination',
+      next: '#timeline .pagination .next_link',
+      triggerPageThreshold: 5
+   });
+
+   ias.extension(new IASTriggerExtension({
+      text: 'Ver más',
+      offset: 3
+   }));
+
+   ias.extension(new IASSpinnerExtension({
+      src: URL+'/assets/images/ajax-loader.gif'
+   }));
+   ias.extension(new IASNoneLeftExtension({
+      text: 'No hay más públicaciones'
+   }));
+
+   ias.on('ready',function(event){
+      buttons();
+   });
+   ias.on('rendered',function(event){
+      buttons();
+   });
+});
+
+function buttons(){
+
+}
