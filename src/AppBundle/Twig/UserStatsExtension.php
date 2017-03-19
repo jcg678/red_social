3696a3da8b5885f3fbac8ae2747eb2e424be4de8
jcg678 <jcg678@hotmail.es>
2017-03-19 13:43:09 +0100

    estadisticas usuario en extension

---
+++ src/AppBundle/Twig/UserStatsExtension.php
@@ -0,0 +1,40 @@
+<?php
+namespace AppBundle\Twig;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class UserStatsExtension extends \Twig_Extension
+{
+    protected $doctrine;
+
+    public function __construct(RegistryInterface $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    public function getFilters(){
+        return array(
+            new \Twig_SimpleFilter('user_stats',array($this,'userStatsFilter'))
+        );
+    }
+
+    public function userStatsFilter($user){
+        $like_repo = $this->doctrine->getRepository('BackendBundle:Like');
+        $following_repo = $this->doctrine->getRepository('BackendBundle:Following');
+        $publication_repo = $this->doctrine->getRepository('BackendBundle:Publication');
+        $user_following= $following_repo->findBy(array('user' => $user));
+        $user_followers= $following_repo->findBy(array('followed' => $user));
+        $user_publications = $publication_repo->findBy(array('user' => $user));
+        $user_likes = $like_repo->findBy(array('user' => $user));
+
+        return $result = array(
+            'following'=>count($user_following),
+            'followers' => count($user_followers),
+            'publications'=> count($user_publications),
+            'likes'=>count($user_likes)
+        );
+    }
+
+    public function  getName(){
+        return 'user_stats_extension';
+    }
+}
