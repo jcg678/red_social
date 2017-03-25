3dec277dc9b9fb499741ffece37d5196bd1e7682
jcg678 <jcg678@hotmail.es>
2017-03-25 11:20:15 +0100

    likes listado

--- src/AppBundle/Controller/LikeController.php
+++ src/AppBundle/Controller/LikeController.php
@@ -55,4 +55,33 @@ class LikeController extends Controller{
         return new Response($status);
     }
 
+    public function likesAction(Request $request, $nickname = null){
+        $em = $this->getDoctrine()->getManager();
+        if($nickname != null){
+            $user_repo = $em->getRepository('BackendBundle:User');
+            $user = $user_repo->findOneBy(array("nick"=> $nickname));
+        }else{
+            $user = $this->getUser();
+        }
+
+        if(empty($user) || !is_object($user)){
+            return $this->redirect($this->generateUrl('home_publications'));
+        }
+
+        $user_id = $user->getId();
+        $dql = "SELECT l FROM BackendBundle:Like l WHERE l.user = $user_id ORDER BY l.id DESC";
+        $query = $em->createQuery($dql);
+        $paginator = $this->get('knp_paginator');
+        $publications = $paginator->paginate($query,
+            $request->query->getInt('page',1),
+            5
+        );
+
+        return $this->render('AppBundle:Like:Likes.html.twig',array(
+            'user'=>$user,
+            'pagination'=>$publications,
+
+        ));
+    }
+
 }
