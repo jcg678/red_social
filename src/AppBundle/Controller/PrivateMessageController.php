8f7e5a4d9a4bc28140b7146385eff36c29cbf705
jcg678 <jcg678@hotmail.es>
2017-04-03 16:20:05 +0200

    guardando los mensajes

--- src/AppBundle/Controller/PrivateMessageController.php
+++ src/AppBundle/Controller/PrivateMessageController.php
@@ -27,6 +27,55 @@ class PrivateMessageController extends Controller{
             'empty_data'=>$user
         ));
 
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            if($form->isValid()){
+                $file=$form['image']->getData();
+                if(!empty($file)){
+                    $ext = $file->guessExtension();
+                    if($ext =='jpg'||$ext == 'jpeg' || $ext == 'png' || $ext == 'gif'){
+                        $file_name = $user->getId().time().".".$ext;
+                        $file->move("uploads/messages/images", $file_name);
+                        $private_message->setImage($file_name);
+                    }else{
+                        $private_message->setImage(null);
+                    }
+                }else{
+                    $private_message->setImage(null);
+                }
+
+
+                $doc=$form['file']->getData();
+                if(!empty($doc)){
+                    $ext = $doc->guessExtension();
+                    if($ext =='doc'||$ext == 'txt' || $ext == 'pdf' || $ext == 'docx'){
+                        $file_name = $user->getId().time().".".$ext;
+                        $doc->move("uploads/message/documents", $file_name);
+                        $private_message->setFile($file_name);
+                    }else{
+                        $private_message->setFile(null);
+                    }
+                }else{
+                    $private_message->setFile(null);
+                }
+                $private_message->setEmitter($user);
+                $private_message->setCreatedAt(new \DateTime("now"));
+                $private_message->setReaded(0);
+                $em->persist($private_message);
+                $flush =$em->flush();
+                if($flush){
+                    $status= "El mensaje no se ha enviado" ;
+                }else{
+                    $status = "El mensaje se ha enviado correactamente";
+                }
+
+            }else{
+                $status = "El mensaje no se ha enviado";
+            }
+            $this->session->getFlashBag()->add("status",$status);
+            return $this->redirectToRoute("private_message_index");
+        }
+
         return $this->render('AppBundle:PrivateMessage:index.html.twig',array(
             'form'=>$form->createView()
         ));
